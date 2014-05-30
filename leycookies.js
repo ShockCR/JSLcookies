8e11b36ec427f41a78f0bfc0d9d5b84a05856eb1
Pablo T. Ruiz <pablotomasruiz@gmail.com>
2014-05-30 10:14:36 +0200

    Update leycookies.js

--- leycookies.js
+++ leycookies.js
@@ -1,32 +1,32 @@
 //***** JS adaptación ley cookies
 
 //*** comprueba que es la primera vez que accede el usuario al sitio
-if (localStorage.cookiesTecnoblogic==undefined){ 
-	$("#avisoCookies").slideToggle(); //muesta aviso
+if (localStorage.cookiesTecnoblogic==undefined){
+	document.getElementById("avisoCookies").style.display="block"; //muesta aviso
 	window.onscroll = hayScroll;
-	var numScrolls=0;	
+	var numScrolls=0;
 }
 
 function hayScroll (){
 	numScrolls++;
-	if (numScrolls>3){	//a partir de 4 acciones de scroll, lanza el temporizador	
-		window.onscroll=null; 
+	if (numScrolls>3){	//a partir de 4 acciones de scroll, lanza el temporizador
+		window.onscroll=null;
 		setTimeout(function(){continuaNavegando()},5000); //pasados 5 seg, quita el aviso y acepta cookies
 	}
 }
 
 //*** quita aviso y carga JS cookies
-function continuaNavegando(){	
-	localStorage.cookiesTecnoblogic="ok"; //para próximas visitas	
-	$("#avisoCookies").slideUp(); //oculta aviso
+function continuaNavegando(){
+	localStorage.cookiesTecnoblogic="ok"; //para próximas visitas
+	document.getElementById("avisoCookies").style.display="none"; //oculta aviso
 	//carga Google Analytics
-	loadJS('funciones', '/analytics.js');
+	loadJS('funciones', 'analytics.js');
 }
 
 //*** carga js dinámicamente
 function loadJS(id, src) {
     if (document.getElementById(id) != null) return;
-    var js = document.createElement('script'); 
+    var js = document.createElement('script');
     js.id = id; js.async = false; js.src = src;
     document.getElementsByTagName('head')[0].appendChild(js);
 }
